55af15485ef58d863e8e724fe90ef445c047f4aa
OverFitted <kozak.bor@gmail.com>
2023-04-26 09:01:13 +0300

    Feat: add lab1 res writer

--- routes/lab.js
+++ routes/lab.js
@@ -4,67 +4,111 @@ const {
 const CLIENT = require('../models/connector');
 const router = Router()
 
+const languageHasher = {
+    "C1": 0,
+    "PHP": 1,
+    "HTML": 2,
+    "0": 0,
+    "1": 1,
+    "2": 2
+}
+
+
+router.get('/lab1', (req, res, next) => {
+    res.status(200)
+
+    CLIENT.query(`select * from users where is_expert='t'`).then(result => {
+        res.render('lab1', {
+            title: "Первая лаба | без CHATGPT",
+            experts: result.rows,
+            isLoggedIn: req.cookies.usr_id
+        })
+    })
+})
+
+router.post('/lab1', (req, res, next) => {
+    res.status(200)
+    var selectedLanguage = languageHasher[req.body.selectedLanguage]
+    res.redirect(`/labs/pvk?selectedLanguage=${selectedLanguage}`)
+})
+
 router.get('/pvk', (req, res, next) => {
     res.status(200)
+
+    var selectedLanguage = languageHasher[req.query.selectedLanguage]
 
     CLIENT.query('select * from pvk_lab1').then(result => {
         res.render('pvk', {
             title: "ПВК | без CHATGPT",
             pvks: result.rows,
-            isLoggedIn: req.cookies.usr_id
+            isLoggedIn: req.cookies.usr_id,
+            selectedLanguage: selectedLanguage
         })
     });
 })
 
 router.post('/pvk', (req, res, next) => {
     res.status(200)
+
+    var selectedLanguage = languageHasher[req.body.selectedLanguage]
+    delete req.body.selectedLanguage;
+
     chosenPvks = Object.keys(req.body)
+    chosenPvksIds = chosenPvks.map(obj => parseInt(obj));
+
+    CLIENT.query(`select * from pvk_lab1 where id in (${chosenPvksIds.toString()})`).then(result => {
+        res.redirect(`/labs/pvk2?chosenPvks=${chosenPvks.join("-")}&selectedLanguage=${selectedLanguage}`)
+    });
+})
+
+router.get('/pvk2', (req, res, next) => {
+    res.status(200)
+
+    var chosenPvks = req.query.chosenPvks.split("-")
+    var selectedLanguage = parseInt(req.query.selectedLanguage)
     chosenPvksIds = chosenPvks.map(obj => parseInt(obj));
 
     CLIENT.query(`select * from pvk_lab1 where id in (${chosenPvksIds.toString()})`).then(result => {
         res.render('pvk2', {
             title: "ПВК | без CHATGPT",
+            isLoggedIn: req.cookies.usr_id,
             pvks: result.rows,
-            chosenPvks: chosenPvks.join("-"),
-            isLoggedIn: req.cookies.usr_id
+            selectedLanguage: selectedLanguage
         })
     });
 })
 
-
-router.get('/lab1', (req, res, next) => {
+router.post('/pvk2', (req, res, next) => {
     res.status(200)
-    res.render('lab1', {
-        title: "Первая лаба | без CHATGPT",
-        experts: [
-            { "name": "Ванька" },
-            { "name": "Компас юзер" },
-            { "name": "Кто-нибудь еще" }
-        ],
-        isLoggedIn: req.cookies.usr_id
-    })
+
+    var expert_id = req.cookies.usr_id;
+    var selectedLanguage = languageHasher[req.body.selectedLanguage]
+    delete req.body.selectedLanguage;
+
+    chosenPvks = Object.keys(req.body)
+    chosenPvksIds = chosenPvks.map(obj => parseInt(obj));
+
+    console.log(req.body)
+    console.log(chosenPvks)
+    console.log(chosenPvksIds)
+
+    chosenPvksIds.forEach(pvk => {
+        const query = {
+            text: 'INSERT INTO expert_profession_quality_lab1 (expert_id, profession_id, pvk_id, importance) VALUES ($1, $2, $3, $4)',
+            values: [expert_id, selectedLanguage, pvk, req.body[pvk.toString()]],
+        }
+
+        CLIENT.query(query).then((res) => {
+            confirm(res)
+        })
+    });
+
+    res.redirect('/labs/lab1/res')
 })
 
 router.get('/lab1/res', (req, res, next) => {
     res.status(200)
     res.redirect("../lab1")
-})
-
-router.post('/lab1/res', (req, res, next) => {
-    chosenPvks = req.query.pvks.split("-")
-    chosenPvksIds = chosenPvks.map(obj => parseInt(obj));
-    console.log(chosenPvksIds)
-
-    res.status(200)
-    res.render('lab1', {
-        title: "Первая лаба | без CHATGPT",
-        experts: [
-            { "name": "Ванька" },
-            { "name": "Компас юзер" },
-            { "name": "Кто-нибудь еще" }
-        ],
-        isLoggedIn: req.cookies.usr_id
-    })
 })
 
 router.get('/lab2', (req, res, next) => {
