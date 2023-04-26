04331e7b24dea7e0c3838072944a7eedd7d7b38a
aleksandr.pevzner <aleksandr.pevzner1@yandex.ru>
2023-04-26 16:35:55 +0300

    Front

--- views/includes/form_20971.php
+++ views/includes/form_20971.php
@@ -8,13 +8,14 @@
 	$email5_31692_7100_20971 = $_POST['email5_31692_7100_20971'];
 	$email5_31692_7100_20971 = $_POST['email5_31692_7100_20971'];
 	$email5_31692_7100_20971 = $_POST['email5_31692_7100_20971'];
+	$input_1538 = $_POST['input_1538'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Name5_31692_7100_20971: $name5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \n";
+				  "Name5_31692_7100_20971: $name5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \nEmail5_31692_7100_20971: $email5_31692_7100_20971 \nInput_1538: $input_1538 \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\r\n";
 	$headers .= "Reply-To: $email5_31692_7100_20971";	
--- views/includes/form_41372.php
+++ views/includes/form_41372.php
@@ -9,13 +9,14 @@
 	$email5_31692_7100_20971_41372 = $_POST['email5_31692_7100_20971_41372'];
 	$email5_31692_7100_20971_41372 = $_POST['email5_31692_7100_20971_41372'];
 	$email5_31692_7100_20971_41372 = $_POST['email5_31692_7100_20971_41372'];
+	$input_1234 = $_POST['input_1234'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Select_51: $select_51 \nName5_31692_7100_20971_41372: $name5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \n";
+				  "Select_51: $select_51 \nName5_31692_7100_20971_41372: $name5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \nEmail5_31692_7100_20971_41372: $email5_31692_7100_20971_41372 \nInput_1234: $input_1234 \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\r\n";
 	$headers .= "Reply-To: $email5_31692_7100_20971_41372";	
